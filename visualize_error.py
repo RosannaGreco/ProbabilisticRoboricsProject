error computation fixed + outputs updated

visualize_error.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import re
 
 x_err = []
 y_err = []
 z_err = []
-qx_err = []
-qy_err = []
-qz_err = []
-qw_err = []
+roll = []
+pitch = []
+yaw = []
+
 
 with open("output/error.dat", "r") as file:
     lines = file.readlines()
+
+def get_roll_pitch_yaw(qx,qy,qz,qw):
+    r = np.arctan2(2*(qw*qx + qy*qz), 1- 2*(qx*qx + qy*qy))
+    p = np.arcsin(2*(qw*qy - qz*qx))
+    y = np.arctan2(2*(qw*qz + qx*qy), 1- 2*(qy*qy + qz*qz))
+    return r, p, y
 
 for line in lines:
     parts = line.split()
@@ -19,17 +26,28 @@ for line in lines:
         x_err.append(float(parts[3]))
         y_err.append(float(parts[4]))
         z_err.append(float(parts[5]))
-        qx_err.append(float(parts[6]))
-        qy_err.append(float(parts[7]))
-        qz_err.append(float(parts[8]))
-        qw_err.append(float(parts[9]))
+
+        qx = float(parts[6])
+        qy = float(parts[7])
+        qz = float(parts[8])
+        qw = float(parts[9])
+        
+        r,p,y = get_roll_pitch_yaw(qx,qy,qz,qw)
+        roll.append(r)
+        pitch.append(p)
+        yaw.append(y)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-#fig, ax = plt.subplots()
 
-ax.plot(qz_err, label="qz error", color="magenta")
+
+#ax.plot(x_err, label="x error", color="blue")
+#ax.plot(y_err, label="y error", color="green")
+#ax.plot(z_err, label="z error", color="red")
+#ax.plot(roll, label="roll", color="orange")
+#ax.plot(pitch, label="pitch", color="blueviolet")
+ax.plot(yaw, label="yaw", color="magenta")
 
 ax.set_xlabel("epoch")
 ax.set_ylabel("error")